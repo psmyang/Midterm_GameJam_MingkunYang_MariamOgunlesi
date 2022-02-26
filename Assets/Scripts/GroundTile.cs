0d8220396579d2f51c25fc333d25edc408337c6f
psmyang <ymk1234567890ymk@sina.com>
2022-02-26 02:22:10 -0500

    Added Ground Spawner

---
+++ Assets/Scripts/GroundTile.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+
+    GroundSpawner groundSpawner;
+
+    private void Start()
+    {
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        groundSpawner.SpawnTile();
+        Destroy(gameObject, 2);
+    }
+
+    private void Update()
+    {
+
+    }
+}
